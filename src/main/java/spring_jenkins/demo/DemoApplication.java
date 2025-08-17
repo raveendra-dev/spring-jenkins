c993cb183b4a3a83624165ef4d18cbc0792e2031
raveendra <raveendra2.g@gmail.com>
2025-08-17 11:48:04 +0530

    first commit

---
+++ src/main/java/spring_jenkins/demo/DemoApplication.java
@@ -0,0 +1,18 @@
+package spring_jenkins.demo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public Logger logger = LoggerFactory.getLogger(DemoApplication.class);
+
+	public static void main(String[] args) {
+		
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/spring_jenkins/demo/DemoApplicationTests.java
@@ -0,0 +1,22 @@
+package spring_jenkins.demo;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+	
+	public Logger logger = LoggerFactory.getLogger(DemoApplicationTests.class);
+
+
+	@Test
+	void contextLoads() {
+		logger.info("Testcase executing");
+		assertEquals(true, true);
+	}
+
+}
